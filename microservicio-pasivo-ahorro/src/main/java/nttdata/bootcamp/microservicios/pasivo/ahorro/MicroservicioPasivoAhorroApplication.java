88bd89b5f742dda51321b054cdcdfadeb1557ea5
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-18 12:10:22 -0500

    primer commit

---
+++ microservicio-pasivo-ahorro/src/main/java/nttdata/bootcamp/microservicios/pasivo/ahorro/MicroservicioPasivoAhorroApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pasivo.ahorro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPasivoAhorroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPasivoAhorroApplication.class, args);
+	}
+
+}
---
+++ microservicio-pasivo-ahorro/src/test/java/nttdata/bootcamp/microservicios/pasivo/ahorro/MicroservicioPasivoAhorroApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pasivo.ahorro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPasivoAhorroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
